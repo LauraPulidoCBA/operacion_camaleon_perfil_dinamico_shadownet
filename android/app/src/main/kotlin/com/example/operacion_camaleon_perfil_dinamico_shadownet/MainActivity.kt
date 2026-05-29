3181ba47010ac42b92b4a4dd96465bc50ec37341
LauraPulidoCBA <laurapulido.cba.sena@gmail.com>
2026-05-28 21:26:02 -0500

    configurar permisos y ajustes iniciales

--- android/app/src/main/kotlin/com/example/operacion_camaleon_perfil_dinamico_shadownet/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/operacion_camaleon_perfil_dinamico_shadownet/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.operacion_camaleon_perfil_dinamico_shadownet
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
